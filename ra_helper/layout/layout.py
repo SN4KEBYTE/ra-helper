7f8a036127d9cd1b99f4baa4fc7aff2c0a6f3aa0
Timur Kasimov <timurcat7@gmail.com>
2021-01-31 00:15:32 +0700

    Added load layout.

--- ra_helper/layout/layout.py
+++ ra_helper/layout/layout.py
@@ -1,13 +1,14 @@
 import PySimpleGUI as sg
 
 from ra_helper.layout._settings_layout import SETTINGS_LAYOUT
+from ra_helper.layout._load_layout import LOAD_LAYOUT
 
 LAYOUT = [
     [
         sg.TabGroup(
             [
                 [
-                    # sg.Tab('Load', _LOAD_LAYOUT),
+                    sg.Tab('Load', LOAD_LAYOUT),
                     # sg.Tab('View', _VIEW_LAYOUT),
                     # sg.Tab('Characteristics', _CHARACTERISTICS_LAYOUT),
                     # sg.Tab('Scatter plots', _SCATTER_LAYOUT),
