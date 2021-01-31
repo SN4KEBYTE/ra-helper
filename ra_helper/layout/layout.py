5977c90cfc5984d74af842e5e97ddfccd8d2fd06
Timur Kasimov <timurcat7@gmail.com>
2021-01-31 14:48:19 +0700

    Added heatmap tab.

--- ra_helper/layout/layout.py
+++ ra_helper/layout/layout.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 
 from ra_helper.layout._settings_layout import SETTINGS_LAYOUT
 from ra_helper.layout._load_layout import LOAD_LAYOUT
+from ra_helper.layout._heatmap_layout import HEATMAP_LAYOUT
 
 LAYOUT = [
     [
@@ -12,6 +13,7 @@ LAYOUT = [
                     # sg.Tab('View', _VIEW_LAYOUT),
                     # sg.Tab('Characteristics', _CHARACTERISTICS_LAYOUT),
                     # sg.Tab('Scatter plots', _SCATTER_LAYOUT),
+                    sg.Tab('Heatmap', HEATMAP_LAYOUT, disabled=True),
                     sg.Tab('Settings', SETTINGS_LAYOUT),
                 ]
             ]
