292de094a1375bf7492037a41187700dac5e63a4
Timur Kasimov <timurcat7@gmail.com>
2021-01-31 14:48:02 +0700

    Added heatmap settings layout.

--- ra_helper/layout/_settings_layout.py
+++ ra_helper/layout/_settings_layout.py
@@ -1,19 +1,55 @@
 import PySimpleGUI as sg
 
-_APPEARANCE_LAYOUT = [
+_GLOBAL_APPEARANCE_LAYOUT = [
     [
-        sg.Text('Appearance'),
+        sg.Text('Global Appearance'),
     ],
     [
         sg.InputOptionMenu(('Light', 'Dark')),
     ],
 ]
 
+_HEATMAP_LAYOUT = [
+    [
+        sg.Text('Heatmap'),
+    ],
+    [
+        sg.Text('kwargs dict (will override defaults params)'),
+        sg.In('{}', key='-HMAP KWARGS-'),
+    ],
+    # [
+    #     sg.Checkbox('Enable annotations', default=True, key='hmap-annotations'),
+    # ],
+    # [
+    #     sg.Text('Format', size=(15, 1)),
+    #     sg.In('.2g', key='-HMAP FMT-'),
+    # ],
+    # [
+    #     sg.Text('Scale minimum', size=(15, 1)),
+    #     sg.In('-1', key='-HMAP VMIN-'),
+    # ],
+    # [
+    #     sg.Text('Scale maximum', size=(15, 1)),
+    #     sg.In('1', key='-HMAP VMAX-'),
+    # ],
+    # [
+    #     sg.Text('Scale center', size=(15, 1)),
+    #     sg.In('1', key='-HMAP CENTER-'),
+    # ],
+    # [
+    #     sg.Text('Form', size=(15, 1)),
+    #     sg.InputOptionMenu(('Square', 'Lower triangle', 'Upper triangle'), key='-HMAP FORM-'),
+    # ],
+]
+
 SETTINGS_LAYOUT = [
     [
-        sg.Column(_APPEARANCE_LAYOUT),
+        sg.Column(_GLOBAL_APPEARANCE_LAYOUT),
     ],
     [
         sg.HorizontalSeparator(),
+    ],
+    [
+        sg.Column(_HEATMAP_LAYOUT),
     ],
 ]
